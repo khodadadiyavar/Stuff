ea46ed46584ca51e428b91f5c1d8625e3d3287f2
Yavar Khodadadijokar <yavar@nebius.com>
2026-06-17 13:17:22 +0200

    feat: add session12 group membership and passwords

--- HomeWorks/session12/prepare-lab.sh
+++ HomeWorks/session12/prepare-lab.sh
@@ -40,5 +40,9 @@ fi
 sudo usermod -u 2012 -g session12_cannon -d /home/session12_chris -m -s /bin/sh -c "Session 12 Analyst" session12_chris
 sudo mkdir -p /home/session12_chris
 sudo chown session12_chris:session12_cannon /home/session12_chris
+sudo usermod -aG session12_mercury,session12_sapphire session12_chris
+
+echo 'session12_alex:session12321' | sudo chpasswd
+echo 'session12_chris:session12321' | sudo chpasswd
 
 echo "Session 12 prep starting..."
